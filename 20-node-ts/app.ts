7ca335c5f34eed36131d610aa2faa39c17a614c1
Lesia Soloviova <solovyova.lesya@gmail.com>
2026-04-24 22:14:03 +0100

    add sections 18-20 about TS for libs, React and Node.js

---
+++ 20-node-ts/app.ts
@@ -0,0 +1,25 @@
+import express, { type Request, type Response, type NextFunction } from 'express';
+
+import todoRoutes from './routes/todo.ts';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(todoRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: 'An error occurred!' });
+});
+
+app.listen(3000);
+
+
+// import {createServer} from 'node:http';
+
+// const server = createServer((req, res) => {
+//   console.log(req.method);
+//   res.end('Hello World!');
+// })
+
+// server.listen(3000)
